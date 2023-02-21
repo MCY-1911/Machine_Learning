7e80a5aed271ddf6375a43d744413e8fb2196ec6
MCY-1911 <al415647@uji.es>
2023-02-21 12:32:24 +0100

    Falta constructor Row

--- src/main/java/PR1/Table.java
+++ src/main/java/PR1/Table.java
@@ -1,11 +1,26 @@
 package PR1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
 
     protected List<String> headers;
     protected List<Row> instancias;
+
+    public Table(String[] cabecera) {
+        instancias = new ArrayList<>();
+        for(String campo : cabecera)
+            headers.add(campo);
+        instancias = new ArrayList<>();
+    }
+
+    public void add(String[] entrada) {
+        List<String> instancia = new ArrayList<>();
+        for(String valor: entrada)
+            instancia.add(valor);
+        //instancias.add(instancia);
+    }
 
     public Row getRowAt(int index) {
         return instancias.get(index);

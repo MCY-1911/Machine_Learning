22e0b4741367f6723780fe22eb0b8b5a5bb2080f
MCY-1911 <al415647@uji.es>
2023-03-09 20:36:39 +0100

    Extraído método  getDistancia()

--- src/main/java/Algoritmos/Kmeans.java
+++ src/main/java/Algoritmos/Kmeans.java
@@ -27,7 +27,7 @@ public class Kmeans {
         // El número que representa el centroide corresponderá con su posición en la lista
         Map<Row, Set<Integer>> asignaciones = new HashMap<>();
         ArrayList<Row> CentroidesOrdenados = new ArrayList<>();
-        
+
 
         // Repetimos el algoritmo numIteraciones veces
         for(int i = 0; i < numIteraciones ; i++) {
@@ -94,6 +94,16 @@ public class Kmeans {
             centroidesEscogidos.add(datos.getRowAt(indiceCentroideActual));
         }
         return centroidesEscogidos;
+    }
+
+    private double getDistanciaEuclidiana(List<Double> vector1, List<Double> vector2) {
+        // Supondremos que ambos vectores tienen el mismo tamaño
+        double distancia = 0.0;
+        for (int i = 0; i < vector1.size(); i++) {
+            distancia += Math.pow(vector1.get(i)- vector2.get(i), 2);
+        }
+        distancia = Math.sqrt(distancia);
+        return distancia;
     }
 
 }

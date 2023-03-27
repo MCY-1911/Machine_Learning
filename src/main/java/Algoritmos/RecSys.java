c7f916e83a8e2831d78cf83da628ac1c95747813
MCY-1911 <al415647@uji.es>
2023-03-27 20:56:10 +0200

    RecSYS refactorizado

--- src/main/java/Algoritmos/RecSys.java
+++ src/main/java/Algoritmos/RecSys.java
@@ -12,12 +12,14 @@ public class RecSys {
 
     Algorithm algoritmo; // Patrón de diseño: Estrategia
     List<String> titulos; // Títulos de las canciones, películas, videojuegos, etc. de nuestra plataforma
-    List<Integer> grupoDeCadaTitulo;
-    Table TablaDatosTítulos; // Contiene datos numéricos para caracterizar a cada titulo
+    List<Integer> identificadorGrupoDeCadaTitulo; // "Columna" de labels. El titulo i-ésimo de la tabla tendrá
+                                                  // por identificador el Integer que ocupe la posición
+                                                  //  i-ésima de la lista
+    Table tablaDatosTitulos; // Contiene datos numéricos para caracterizar a cada titulo
 
     public RecSys(Algorithm algorithm){
         this.algoritmo = algorithm;
-        this.grupoDeCadaTitulo = new ArrayList<>();
+        this.identificadorGrupoDeCadaTitulo = new ArrayList<>();
     }
 
     public void train(Table trainData) throws MasDatosQueGruposException {
@@ -25,62 +27,62 @@ public class RecSys {
     }
 
     public void run(Table testData, List<String> testItemNames){
-        // Este método simplemente debe etiquetar los datos.
-        // Por ejemplo, las canciones. testData será una tabla donde cada fila representa una canción
+        // Este método simplemente debe etiquetar los datos. Es decir,
+        // que las canciones, por ejemplo, que son similares entre sí pertenezcan al mismo grupo.
         // Los datos de cada canción puede ser su duración, sus likes, su velocidad, etc.
+
         // testItemNames simplemente contiene los títulos de las canciones.
-        // Vale, cuando llamamos a este método lo que queremos conseguir es que las canciones se etiqueten. Es decir,
-        // que las canciones que son similares entre sí pertenezcan al mismo grupo y,
-        // por lo tanto, tengan la misma estimación.
         this.titulos = testItemNames;
-        this.TablaDatosTítulos = testData;
-        // Esto sí son atributos de nuestra plataforma, ahora iniciada, debemos estimar el "grupo" de cada canción
-        // OJO, no sé el cast a Integer si es correcto. PREGUNTAR PROFE !!!!!!!!!!!!
-        for(Row datosTitulo : testData.getInstancias()) {
-            grupoDeCadaTitulo.add((Integer) algoritmo.estimate(datosTitulo.getData()));
-        }
+        this.tablaDatosTitulos = testData;
+
+        // Método auxiliar, estima el grupo de TODOS los datos
+        this.estimate();
 
     }
 
     public List<String> recommend(String nameLikedItem, int numRecommendations){
+
         // Paso 1: obtenemos los datos del título marcado como Liked
-        int indice = findName(nameLikedItem);
-        List<Double> datosTitulo = TablaDatosTítulos.getRowAt(indice).getData();
+        int indiceLiked = findName(nameLikedItem);
 
         // Paso 2: obtenemos el identificador del grupo de dicha canción Liked
-        Integer grupo = grupoDeCadaTitulo.get(indice);
+        Integer labelLiked = identificadorGrupoDeCadaTitulo.get(indiceLiked);
 
-        // Paso 3: localizar titulos del grupo HE LLEGADO HASTA AQUÍ
-        List<Integer> listaDeIndices = new ArrayList<>();
-        int i = 0;
-        while(i < TablaDatosTítulos.getNumeroFilas() && listaDeIndices.size() < numRecommendations) {
+        // Paso 3: localizar títulos del grupo
+        List<Integer> listaDeIndices = selectItems(indiceLiked, labelLiked, numRecommendations);
 
-
-            i++;
-        }
-        //Encontramos los indices de todos los datos que tengan el mismo grupo
-        for (int i = 0; i< grupoDeCadaTitulo.size(); i++ ){
-            if (grupoDeCadaTitulo.get(i) == grupo && indice != i){
-                listaDeIndices.add(i);
-                if(listaDeIndices.size() == numRecommendations){
-                    break;
-                }
-            }
-        }
-        //Asignamos el nombre correspondiente de cada indice
-        List<String> recomendaciones = new ArrayList<>();
-        for (Integer integer : listaDeIndices){
-            recomendaciones.add(titulos.get(integer));
-        }
-
-        return recomendaciones;
+        //Asignamos el nombre correspondiente de cada indice y devolvemos
+        return getNamesSelectedItems();
     }
 
     private int findName(String nombre){
         return titulos.indexOf(nombre);
     }
 
+    private void estimate(){
+        // OJO, no sé el cast a Integer si es correcto. PREGUNTAR PROFE !!!!!!!!!!!!
+        for(Row datosTitulo:  tablaDatosTitulos.getInstancias())
+            identificadorGrupoDeCadaTitulo.add((Integer) algoritmo.estimate(datosTitulo.getData()));
+    }
 
+    private List<Integer> selectItems(int indiceLiked, int labelLiked, int numRecomendaciones){
+        List<Integer> listaDeIndices = new ArrayList<>();
+        for (int indiceCandidato = 0; indiceCandidato < identificadorGrupoDeCadaTitulo.size(); indiceCandidato++) {
+            Integer labelCandidato = identificadorGrupoDeCadaTitulo.get(indiceCandidato);
+            if (labelCandidato == labelLiked && indiceCandidato != indiceLiked)
+                listaDeIndices.add(indiceCandidato);
+            if (listaDeIndices.size() >= numRecomendaciones)
+                break;
+        }
+        return listaDeIndices;
+    }
+
+    private List<String> getNamesSelectedItems(List<Integer> listaIndices) {
+        List<String> listaDeNombres = new ArrayList<>(listaIndices.size());
+        for (Integer indice : listaIndices)
+            listaDeNombres.add(titulos.get(indice));
+        return listaDeNombres;
+    }
 
 
 }

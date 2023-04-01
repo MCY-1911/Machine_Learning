ba6ba007bd15e838abf704bc4f765d79b4f67770
MCY-1911 <al415647@uji.es>
2023-04-01 20:12:02 +0200

    Pruebas Kmeans modificadas

--- src/test/java/Algoritmos/KmeansTest.java
+++ src/test/java/Algoritmos/KmeansTest.java
@@ -1,12 +1,15 @@
 package Algoritmos;
 
 import Exceptions.MasDatosQueGruposException;
+import TratamientoDatos.Filas.Row;
 import TratamientoDatos.Lectores.CSV;
 import TratamientoDatos.Tablas.Table;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,37 +19,80 @@ class KmeansTest {
 
     CSV constructorTablas = new CSV();
     // Con esta semilla los puntos el centroide del grupo de puntos del 3º Cuadrante corresponde al 1
-    Kmeans estimador2Clusters = new Kmeans(2, 10, 4l);
+    Kmeans estimador2Clusters = new Kmeans(2, 20, 4l);
     Kmeans estimador28Clusters = new Kmeans(28,10, 4);
-    List<Double> punto1rCuadrante = new ArrayList();
-    List<Double> punto3rCuadrante = new ArrayList();
+
 
     Table TablaGruposPrimerYTercerCuadrante;
 
-    @BeforeEach
-    void setUp() throws MasDatosQueGruposException {
+
+     public KmeansTest() throws MasDatosQueGruposException {
         TablaGruposPrimerYTercerCuadrante = constructorTablas.readTable("src" + File.separator + "Files" +File.separator + "PruebaKmeans.csv");
         estimador2Clusters.train(TablaGruposPrimerYTercerCuadrante);
+         try {
+             saveTableWithPredictions(TablaGruposPrimerYTercerCuadrante,  "src" + File.separator + "Files" +File.separator + "PruebaKmeans"+ "_out.csv", estimador2Clusters);
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
 
     }
 
 
     @Test
     void estimatePunto1ºCuadrante()  {
+        List<Double> punto1rCuadrante = new ArrayList();
         punto1rCuadrante.add(20.0);
         punto1rCuadrante.add(22.0);
+        System.out.println(estimador2Clusters.estimate(punto1rCuadrante));
         assertEquals(2, estimador2Clusters.estimate(punto1rCuadrante));
     }
 
     @Test
     void estimatePunto3ºCuadrante()  {
+        List<Double> punto3rCuadrante = new ArrayList();
         punto3rCuadrante.add(-7.0);
         punto3rCuadrante.add(-7.0);
         assertEquals(1, estimador2Clusters.estimate(punto3rCuadrante));
     }
 
     @Test
+    void estimatePuntosDiferentesCuadrantes() {
+        List<Double> punto1rCuadrante = new ArrayList();
+        punto1rCuadrante.add(20.0);
+        punto1rCuadrante.add(22.0);
+        List<Double> punto3rCuadrante = new ArrayList();
+        punto3rCuadrante.add(-7.0);
+        punto3rCuadrante.add(-7.0);
+        assertNotEquals(estimador2Clusters.estimate(punto1rCuadrante), estimador2Clusters.estimate(punto3rCuadrante));
+
+    }
+
+    @Test
     void TestExcepción() throws MasDatosQueGruposException {
         assertThrows(MasDatosQueGruposException.class, () -> estimador28Clusters.train(TablaGruposPrimerYTercerCuadrante));
     }
+
+    public void saveTableWithPredictions(Table t, String filename, Kmeans estimador) throws IOException {
+        try {
+            FileWriter fw = new FileWriter(filename);
+            for (int i=0; i<t.getNumeroFilas(); i++)
+            {
+                Row row = t.getRowAt(i);
+                List<Double> datos = row.getData();
+                datos.add((double)estimador.estimate(datos));
+                int j=0;
+                for (; j<datos.size()-1; j++)
+                {
+                    fw.write(datos.get(j).toString());
+                    fw.write(",");
+                }
+                fw.write(datos.get(j).toString());
+                fw.write("\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
 }
